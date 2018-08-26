43132039ca10753cbf97e37326bbce6639754bdf
Łukasz Wójcik <kontakt.github@lukaszwojcik.net>
2018-08-26 20:16:53 +0200

    minor fixes

--- helpers/starcraft2/header/index.js
+++ helpers/starcraft2/header/index.js
@@ -1,26 +1,24 @@
 const sc2playerApi = require('../../../api/starcraft2/player');
 
-const formatHeaderData = (data) => {
-  return {
-    player: {
-      server: data.server,
-      name: data.displayName,
-      clan: {
-        name: data.clanName || '',
-        tag: data.clanTag || '',
-      },
-      rank: data.career.highest1v1Rank || '',
-      portrait: {
-        x: typeof data.portrait !== 'undefined' ? data.portrait.x : '',
-        y: typeof data.portrait !== 'undefined' ? data.portrait.y : '',
-        w: typeof data.portrait !== 'undefined' ? data.portrait.w : '',
-        h: typeof data.portrait !== 'undefined' ? data.portrait.h : '',
-        offset: typeof data.portrait !== 'undefined' ? data.portrait.offset : '',
-        url: typeof data.portrait !== 'undefined' ? data.portrait.url : '',
-      },
+const formatHeaderData = data => ({
+  player: {
+    server: data.server,
+    name: data.displayName,
+    clan: {
+      name: data.clanName || '',
+      tag: data.clanTag || '',
     },
-  };
-};
+    rank: data.career.highest1v1Rank || '',
+    portrait: {
+      x: typeof data.portrait !== 'undefined' ? data.portrait.x : '',
+      y: typeof data.portrait !== 'undefined' ? data.portrait.y : '',
+      w: typeof data.portrait !== 'undefined' ? data.portrait.w : '',
+      h: typeof data.portrait !== 'undefined' ? data.portrait.h : '',
+      offset: typeof data.portrait !== 'undefined' ? data.portrait.offset : '',
+      url: typeof data.portrait !== 'undefined' ? data.portrait.url : '',
+    },
+  },
+});
 
 const getHeaderData = async player => sc2playerApi.getPlayerProfile(player)
   .then((data) => {
