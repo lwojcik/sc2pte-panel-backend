b3ccf1623107848082fcf0c7a622980ed6865ac8
Łukasz Wójcik <kontakt.github@lukaszwojcik.net>
2018-09-14 23:01:27 +0200

    fetching config via profile url

--- routes/v1/config.js
+++ routes/v1/config.js
@@ -9,7 +9,7 @@ const router = require('express').Router();
 const cache = require('../../config/cache');
 const apicache = require('apicache').options({ debug: cache.debug }).middleware;
 
-// const { getConfig } = require('../../controllers/config/get');
+const { getConfig } = require('../../controllers/config/get');
 // const saveConfig = require('../../controllers/config/save');
 
 const onlyStatus200 = (req, res) => res.statusCode === 200;
@@ -18,14 +18,10 @@ const cacheSuccessesOnly = apicache(cache.request, onlyStatus200);
 router.get('/get/:channelId', cacheSuccessesOnly, async (req, res) => {
   try {
     const { channelId } = req.params;
-    console.log(req.params); // eslint-disable-line
-    console.log(req.headers); // eslint-disable-line
+    const { token } = req.headers;
+    const response = await getConfig(channelId, token);
 
-    return res.status(201).json({ id: channelId });
-    // const { channelId } = req.params;
-    // const { token } = req.headers;
-    // const response = await getConfig(channelId, token);
-    // return res.status(response.status).json(response);
+    return res.status(response.status).json(response);
   } catch (error) {
     return res.status(400).json({ status: 400, message: 'Bad request' });
   }
