913d1251457e97bdf81ad8487c95a49eec537a00
Łukasz Wójcik <lwojcik@lukem.net>
2019-08-28 21:21:17 +0200

    sample profiles

--- src/routes/config/get/index.ts
+++ src/routes/config/get/index.ts
@@ -6,8 +6,34 @@ export default fp((server, {}, next) => {
     const { channelId } = request.params;
     reply.code(200).send({
       status: 200,
-      message: "getConfig",
       channelId,
+      profiles: [
+        {
+          regionId: 1,
+          realmId: 1,
+          profileId: "1084304",
+        },
+        {
+          regionId: 1,
+          realmId: 1,
+          profileId: "6615271",
+        },
+        {
+          regionId: 2,
+          realmId: 1,
+          profileId: "5593296",
+        },
+        {
+          regionId: 1,
+          realmId: 1,
+          profileId: "2840641",
+        },
+        {
+          regionId: 2,
+          realmId: 1,
+          profileId: "4682128",
+        },
+      ],
     });
   });
   next();
