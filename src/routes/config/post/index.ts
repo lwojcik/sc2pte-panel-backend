3451a5c8a308a37fc2a2da68b5d44b482c6994cd
Łukasz Wójcik <lwojcik@lukem.net>
2019-09-27 21:32:13 +0200

    receiving data object

--- src/routes/config/post/index.ts
+++ src/routes/config/post/index.ts
@@ -4,6 +4,10 @@ import fp from "fastify-plugin";
 export default fp((server, {}, next) => {
   server.post("/config", /* { schema }, */ (request, reply) => {
     const { channelId } = request.params;
+    const auth = request.headers;
+    const data = JSON.parse(request.body);
+    console.log(auth);
+    console.log(data);
     reply.code(200).send({
       status: 200,
       message: "postConfig",
