7996f59a24b50b43ec8d2bb7d4adc0b7e758de9e
Łukasz Wójcik <lwojcik@lukem.net>
2019-09-29 21:36:04 +0200

    minor fixes

--- src/routes/config/post/index.ts
+++ src/routes/config/post/index.ts
@@ -4,11 +4,29 @@ import { saveConfig } from '../../../controllers/config';
 
 export default fp((server, {}, next) => {
   server.post("/config", /* { schema }, */ async (request, reply) => {
-    const { channelid } = request.headers;
-    const data = JSON.parse(request.body);
+    try {
+      const { channelid } = request.headers;
+      const data = JSON.parse(request.body);
 
-    const savedConfigStatus = await saveConfig(channelid, data);
-    reply.code(savedConfigStatus.status).send(savedConfigStatus);
+      const configSaved = await saveConfig(channelid, data);
+      
+      if (configSaved) {
+        reply.code(200).send({
+          status: 200,
+          message: 'Config saved',
+        });
+      } else {
+        reply.code(400).send({
+          status: 400,
+          message: 'Config save failed',
+        });
+      }
+    } catch (error) {
+      reply.code(400).send({
+        status: 400,
+        message: 'Incorrect or malformed request',
+      });
+    }
   });
   next();
 });
