62fd35d6714baed7bcb0a1f4b84079d5a03700c6
Łukasz Wójcik <lwojcik@lukem.net>
2019-11-16 18:26:13 +0100

    mocked viewer response

--- src/routes/viewer/get/index.ts
+++ src/routes/viewer/get/index.ts
@@ -11,9 +11,64 @@ export default fp((server, {}, next) => {
     const { channelId } = request.params;
     reply.code(200).send({
       status: 200,
-      message: "getViewer",
-      date: new Date(),
       channelId,
+      data: {
+        profiles: [
+          {
+            heading: {
+              portrait: {
+                url: "https://static.starcraft2.com/starport/d0e7c831-18ab-4cd6-adc7-9d4a28f49ec7/portraits/2-14.jpg",
+                frame: "grandmaster",
+              },
+              player: {
+                clan: {
+                  name: "Team Name",
+                  tag: "TEAM",
+                },
+                name: "Player name",
+                server: "kr",
+              },
+            },
+            snapshot: [
+              {
+                mode: "1v1",
+                rank: "silver",
+                wins: 101,
+                losses: 100,
+                race: "random",
+                mmr: 3655,
+                divisionRank: 3,
+              },
+            ],
+            stats: {
+              totalGames: 100,
+              bonusPool: 24,
+              highestSoloRank: "silver",
+              highestTeamRank: "",
+            },
+            history: [
+              {
+                mapName: "Kairos Junction LE",
+                mode: "1v1",
+                result: "win",
+                date: new Date(),
+              },
+              {
+                mapName: "Kairos Junction LE",
+                mode: "1v1",
+                result: "loss",
+                date: new Date(),
+              },
+              {
+                mapName: "Kairos Junction LE",
+                mode: "1v1",
+                result: "win",
+                date: new Date(),
+              },
+            ],
+          },
+        ],
+      },
     });
   });
   next();
