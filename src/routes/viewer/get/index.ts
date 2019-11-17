76b93a0256c241c8e7ab53cd7b59f4055bdfbb11
Łukasz Wójcik <lwojcik@lukem.net>
2019-11-17 17:15:34 +0100

    multiple profiles

--- src/routes/viewer/get/index.ts
+++ src/routes/viewer/get/index.ts
@@ -26,7 +26,7 @@ export default fp((server, {}, next) => {
                   tag: "TEAM",
                 },
                 name: "Player name",
-                server: "kr",
+                server: "eu",
               },
             },
             snapshot: [
@@ -44,7 +44,131 @@ export default fp((server, {}, next) => {
                 rank: "silver",
                 wins: 101,
                 losses: 100,
+                race: "terran",
+                mmr: 3655,
+                divisionRank: 3,
+              },
+            ],
+            stats: {
+              totalGames: 100,
+              bonusPool: 24,
+              highestSoloRank: "silver",
+              highestTeamRank: "",
+            },
+            history: [
+              {
+                mapName: "Kairos Junction LE",
+                mode: "1v1",
+                result: "win",
+                date: new Date(),
+              },
+              {
+                mapName: "Kairos Junction LE",
+                mode: "1v1",
+                result: "loss",
+                date: new Date(),
+              },
+              {
+                mapName: "Kairos Junction LE",
+                mode: "1v1",
+                result: "win",
+                date: new Date(),
+              },
+            ],
+          },
+          {
+            heading: {
+              portrait: {
+                url: "https://static.starcraft2.com/starport/d0e7c831-18ab-4cd6-adc7-9d4a28f49ec7/portraits/2-14.jpg",
+                frame: "grandmaster",
+              },
+              player: {
+                clan: {
+                  name: "Team Name",
+                  tag: "TEAM",
+                },
+                name: "Player name",
+                server: "eu",
+              },
+            },
+            snapshot: [
+              {
+                mode: "1v1",
+                rank: "silver",
+                wins: 101,
+                losses: 100,
                 race: "random",
+                mmr: 3655,
+                divisionRank: 3,
+              },
+              {
+                mode: "2v2",
+                rank: "silver",
+                wins: 101,
+                losses: 100,
+                race: "terran",
+                mmr: 3655,
+                divisionRank: 3,
+              },
+            ],
+            stats: {
+              totalGames: 100,
+              bonusPool: 24,
+              highestSoloRank: "silver",
+              highestTeamRank: "",
+            },
+            history: [
+              {
+                mapName: "Kairos Junction LE",
+                mode: "1v1",
+                result: "win",
+                date: new Date(),
+              },
+              {
+                mapName: "Kairos Junction LE",
+                mode: "1v1",
+                result: "loss",
+                date: new Date(),
+              },
+              {
+                mapName: "Kairos Junction LE",
+                mode: "1v1",
+                result: "win",
+                date: new Date(),
+              },
+            ],
+          },
+          {
+            heading: {
+              portrait: {
+                url: "https://static.starcraft2.com/starport/d0e7c831-18ab-4cd6-adc7-9d4a28f49ec7/portraits/2-14.jpg",
+                frame: "grandmaster",
+              },
+              player: {
+                clan: {
+                  name: "Team Name",
+                  tag: "TEAM",
+                },
+                name: "Player name",
+                server: "eu",
+              },
+            },
+            snapshot: [
+              {
+                mode: "1v1",
+                rank: "silver",
+                wins: 101,
+                losses: 100,
+                race: "random",
+                mmr: 3655,
+                divisionRank: 3,
+              },
+              {
+                mode: "2v2",
+                rank: "silver",
+                wins: 101,
+                losses: 100,
+                race: "terran",
                 mmr: 3655,
                 divisionRank: 3,
               },
