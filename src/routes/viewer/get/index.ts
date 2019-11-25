348871865aa258809111c952da28e9c0f055ad1d
Łukasz Wójcik <lwojcik@lukem.net>
2019-11-25 22:20:19 +0100

    reshape viewer route

--- src/routes/viewer/get/index.ts
+++ src/routes/viewer/get/index.ts
@@ -29,302 +29,56 @@ export default fp((server, {}, next) => {
                   server: "eu",
                 },
               },
-              snapshot: [
-                {
-                  mode: "1v1",
-                  rank: "silver",
-                  wins: 101,
-                  losses: 100,
-                  race: "random",
-                  mmr: 3655,
-                  divisionRank: 3,
-                },
-                {
-                  mode: "2v2",
-                  rank: "silver",
-                  wins: 101,
-                  losses: 100,
-                  race: "terran",
-                  mmr: 3655,
-                  divisionRank: 3,
-                },
-              ],
-              stats: {
-                totalGames: 100,
-                bonusPool: 24,
-                highestSoloRank: "silver",
-                highestTeamRank: "",
-              },
-              history: [
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "win",
-                  date: new Date(),
-                },
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "loss",
-                  date: new Date(),
-                },
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "win",
-                  date: new Date(),
-                },
-              ],
-            },
-            {
-              heading: {
-                portrait: {
-                  url: "https://static.starcraft2.com/starport/d0e7c831-18ab-4cd6-adc7-9d4a28f49ec7/portraits/2-14.jpg",
-                  frame: "grandmaster",
-                },
-                player: {
-                  clan: {
-                    name: "",
-                    tag: "",
+              details: {
+                snapshot: [
+                  {
+                    mode: "1v1",
+                    rank: "silver",
+                    wins: 101,
+                    losses: 100,
+                    race: "random",
+                    mmr: 3655,
+                    divisionRank: 3,
                   },
-                  name: "Player name",
-                  server: "eu",
-                },
-              },
-              snapshot: [
-                {
-                  mode: "1v1",
-                  rank: "silver",
-                  wins: 101,
-                  losses: 100,
-                  race: "random",
-                  mmr: 3655,
-                  divisionRank: 3,
-                },
-                {
-                  mode: "2v2",
-                  rank: "silver",
-                  wins: 101,
-                  losses: 100,
-                  race: "terran",
-                  mmr: 3655,
-                  divisionRank: 3,
-                },
-              ],
-              stats: {
-                totalGames: 100,
-                bonusPool: 24,
-                highestSoloRank: "silver",
-                highestTeamRank: "",
-              },
-              history: [
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "win",
-                  date: new Date(),
-                },
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "loss",
-                  date: new Date(),
-                },
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "win",
-                  date: new Date(),
-                },
-              ],
-            },
-            {
-              heading: {
-                portrait: {
-                  url: "https://static.starcraft2.com/starport/d0e7c831-18ab-4cd6-adc7-9d4a28f49ec7/portraits/2-14.jpg",
-                  frame: "grandmaster",
-                },
-                player: {
-                  clan: {
-                    name: "",
-                    tag: "",
+                  {
+                    mode: "2v2",
+                    rank: "silver",
+                    wins: 101,
+                    losses: 100,
+                    race: "terran",
+                    mmr: 3655,
+                    divisionRank: 3,
                   },
-                  name: "Player name",
-                  server: "eu",
+                ],
+                stats: {
+                  totalGames: 100,
+                  bonusPool: 24,
+                  highestSoloRank: "silver",
+                  highestTeamRank: "",
                 },
-              },
-              snapshot: [
-                {
-                  mode: "1v1",
-                  rank: "silver",
-                  wins: 101,
-                  losses: 100,
-                  race: "random",
-                  mmr: 3655,
-                  divisionRank: 3,
-                },
-                {
-                  mode: "2v2",
-                  rank: "silver",
-                  wins: 101,
-                  losses: 100,
-                  race: "terran",
-                  mmr: 3655,
-                  divisionRank: 3,
-                },
-              ],
-              stats: {
-                totalGames: 100,
-                bonusPool: 24,
-                highestSoloRank: "silver",
-                highestTeamRank: "",
-              },
-              history: [
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "win",
-                  date: new Date(),
-                },
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "loss",
-                  date: new Date(),
-                },
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "win",
-                  date: new Date(),
-                },
-              ],
-            },
-            {
-              heading: {
-                portrait: {
-                  url: "https://static.starcraft2.com/starport/d0e7c831-18ab-4cd6-adc7-9d4a28f49ec7/portraits/2-14.jpg",
-                  frame: "grandmaster",
-                },
-                player: {
-                  clan: {
-                    name: "",
-                    tag: "",
+                history: [
+                  {
+                    mapName: "Kairos Junction LE",
+                    mode: "1v1",
+                    result: "win",
+                    date: 1562164424000,
                   },
-                  name: "Player name",
-                  server: "eu",
-                },
-              },
-              snapshot: [
-                {
-                  mode: "1v1",
-                  rank: "silver",
-                  wins: 101,
-                  losses: 100,
-                  race: "random",
-                  mmr: 3655,
-                  divisionRank: 3,
-                },
-                {
-                  mode: "2v2",
-                  rank: "silver",
-                  wins: 101,
-                  losses: 100,
-                  race: "terran",
-                  mmr: 3655,
-                  divisionRank: 3,
-                },
-              ],
-              stats: {
-                totalGames: 100,
-                bonusPool: 24,
-                highestSoloRank: "silver",
-                highestTeamRank: "",
-              },
-              history: [
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "win",
-                  date: new Date(),
-                },
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "loss",
-                  date: new Date(),
-                },
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "win",
-                  date: new Date(),
-                },
-              ],
-            },
-            {
-              heading: {
-                portrait: {
-                  url: "https://static.starcraft2.com/starport/d0e7c831-18ab-4cd6-adc7-9d4a28f49ec7/portraits/2-14.jpg",
-                  frame: "grandmaster",
-                },
-                player: {
-                  clan: {
-                    name: "",
-                    tag: "",
+                  {
+                    mapName: "Kairos Junction LE",
+                    mode: "1v1",
+                    result: "loss",
+                    date: 1562164424000,
                   },
-                  name: "Player name",
-                  server: "eu",
-                },
+                  {
+                    mapName: "Kairos Junction LE",
+                    mode: "1v1",
+                    result: "win",
+                    date: 1562164424000,
+                  },
+                ],
               },
-              snapshot: [
-                {
-                  mode: "1v1",
-                  rank: "silver",
-                  wins: 101,
-                  losses: 100,
-                  race: "random",
-                  mmr: 3655,
-                  divisionRank: 3,
-                },
-                {
-                  mode: "2v2",
-                  rank: "silver",
-                  wins: 101,
-                  losses: 100,
-                  race: "terran",
-                  mmr: 3655,
-                  divisionRank: 3,
-                },
-              ],
-              stats: {
-                totalGames: 100,
-                bonusPool: 24,
-                highestSoloRank: "silver",
-                highestTeamRank: "",
-              },
-              history: [
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "win",
-                  date: new Date(),
-                },
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "loss",
-                  date: new Date(),
-                },
-                {
-                  mapName: "Kairos Junction LE",
-                  mode: "1v1",
-                  result: "win",
-                  date: new Date(),
-                },
-              ],
             },
-          ].slice(0,3),
+          ].slice(0,1),
         },
     });
   });
